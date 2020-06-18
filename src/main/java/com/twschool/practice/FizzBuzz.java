b6e99e6c45a3ec00c02884ffea2c473959550154
jiaoguibin <jiaoguibin@zybank.com.cn>
2020-06-18 15:49:26 +0800

    [feat]finish should_return_Fizz_given_number_75

--- src/main/java/com/twschool/practice/FizzBuzz.java
+++ src/main/java/com/twschool/practice/FizzBuzz.java
@@ -2,17 +2,20 @@ package com.twschool.practice;
 
 public class FizzBuzz {
     public String say(int number) {
-        if (!String.valueOf(number).contains("5") && String.valueOf(number).contains("3")) {
+        String formatNumber = String.valueOf(number);
+
+        if (!formatNumber.contains("5") && formatNumber.contains("3")) {
             return "Fizz";
         }
 
         String result = "";
 
-        if (number % 3 == 0) {
+
+        if (number % 3 == 0 && (!formatNumber.contains("5") || formatNumber.contains("7"))) {
             result += "Fizz";
         }
 
-        if (number % 5 == 0) {
+        if (number % 5 == 0 && !formatNumber.contains("7")) {
             result += "Buzz";
         }
 
--- src/test/java/com/twschool/practice/FizzBuzzTest.java
+++ src/test/java/com/twschool/practice/FizzBuzzTest.java
@@ -30,9 +30,16 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void should_return_fizzBuzz_given_number_15() {
-        String result = fizzBuzz.say(15);
+    public void should_return_Buzz_given_number_60() {
+        String result = fizzBuzz.say(60);
         Assert.assertEquals("FizzBuzz", result);
+    }
+
+
+    @Test
+    public void should_return_Buzz_given_number_15() {
+        String result = fizzBuzz.say(15);
+        Assert.assertEquals("Buzz", result);
     }
 
     @Test
@@ -43,14 +50,20 @@ public class FizzBuzzTest {
 
     @Test
     public void should_return_BuzzWhizz_given_number() {
-        String result = fizzBuzz.say(70);
+        String result = fizzBuzz.say(140);
         Assert.assertEquals("BuzzWhizz", result);
     }
 
     @Test
-    public void should_return_FizzBuzzWhizz_given_number_105() {
-        String result = fizzBuzz.say(105);
+    public void should_return_FizzBuzzWhizz_given_number_210() {
+        String result = fizzBuzz.say(210);
         Assert.assertEquals("FizzBuzzWhizz", result);
+    }
+
+    @Test
+    public void should_return_BuzzWhizz_given_number_105() {
+        String result = fizzBuzz.say(105);
+        Assert.assertEquals("BuzzWhizz", result);
     }
 
     @Test
@@ -72,14 +85,20 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void should_return_FizzBuzz_given_number_135() {
+    public void should_return_Buzz_given_number_135() {
         String result = fizzBuzz.say(135);
-        Assert.assertEquals("FizzBuzz", result);
+        Assert.assertEquals("Buzz", result);
     }
 
     @Test
     public void should_return_Buzz_given_number_25() {
         String result = fizzBuzz.say(25);
         Assert.assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void should_return_Fizz_given_number_75() {
+        String result = fizzBuzz.say(75);
+        Assert.assertEquals("Fizz", result);
     }
 }
