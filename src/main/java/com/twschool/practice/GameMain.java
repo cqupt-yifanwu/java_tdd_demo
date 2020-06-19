0dd1ce9bcc127d51668d5616d6056f2dbedc79de
jiaoguibin <jiaoguibin@zybank.com.cn>
2020-06-19 23:00:47 +0800

    [feat]完善main函数

--- src/main/java/com/twschool/practice/GameMain.java
+++ src/main/java/com/twschool/practice/GameMain.java
@@ -12,10 +12,16 @@ public class GameMain {
         GuessNumberGame guessNumberGame = new GuessNumberGame(gameAnswer);
 
         Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
+
 
         while (guessNumberGame.getStatus().equals(GameStatus.CONTINUE)) {
-            guessNumberGame.guess(text);
+            System.out.println("输入");
+            String text = scanner.nextLine();
+            String result = guessNumberGame.guess(text);
+
+            // todo 对输入字符进行异常情况判断与处理
+
+            System.out.println(result);
         }
     }
 }
